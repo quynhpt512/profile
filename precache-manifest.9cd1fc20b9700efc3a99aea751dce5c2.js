8b289be0d3aa1d9c61458a65050fcfc221dbc51a
Quynh Pham <quynh.p@shopstack.asia>
2019-03-01 11:53:37 +0700

    Updates

---
+++ precache-manifest.9cd1fc20b9700efc3a99aea751dce5c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f550f41b4fe6cf4c3a27b7e9123e089d",
+    "url": "/profile/static/media/avata.f550f41b.png"
+  },
+  {
+    "revision": "4fba1a2fa32e33d0605c",
+    "url": "/profile/static/js/runtime~main.4fba1a2f.js"
+  },
+  {
+    "revision": "3c8a7aa5f3f541d8691e",
+    "url": "/profile/static/js/main.3c8a7aa5.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/profile/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "3c8a7aa5f3f541d8691e",
+    "url": "/profile/static/css/main.31f05f4e.chunk.css"
+  },
+  {
+    "revision": "3ae0ce5efd1391d401b46bb0860c4460",
+    "url": "/profile/index.html"
+  }
+];
